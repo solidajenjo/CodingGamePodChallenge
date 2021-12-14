8fefb505b92bdf5155160001d7648e1def0637d5
Rubén Ajenjo Rodriguez <solidajenjo@gmail.com>
2021-12-14 20:30:44 +0100

    WIP - Register checkpoints on the first lap
    Trying to register the checkpoints on the first lap to improve the turns the rest of the race.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+struct CheckPoint
+{    
+    explicit CheckPoint(int x, int y) : x(x) , y(y) {
+        static int id = 0;
+        checkPointNum = id++;
+    }
+    
+    int x , y;
+    int checkPointNum = 0;
+    CheckPoint* nextCheckPoint { nullptr };
+
+};
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -14,6 +27,11 @@ int main()
 {
 
     // game loop
+
+    CheckPoint* firstCheckPoint { nullptr };
+    CheckPoint* lastCheckPointInserted { nullptr };
+    bool firstLapCompleted = false;
+
     while (1) {
         int x;
         int y;
@@ -26,15 +44,67 @@ int main()
         int opponent_y;
         cin >> opponent_x >> opponent_y; cin.ignore();
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        if (firstCheckPoint != lastCheckPointInserted && !firstLapCompleted){
+            firstLapCompleted = (firstCheckPoint->x == next_checkpoint_x && firstCheckPoint->y == next_checkpoint_y);
+        }
+        if (firstLapCompleted){
+            cerr << "First lap completed!!\n";
+        }
+        else{
+            cerr << "First lap\n";
+            if (!firstCheckPoint){ //Create the checkpoint list head
+                firstCheckPoint = new CheckPoint(next_checkpoint_x, next_checkpoint_y);
+                lastCheckPointInserted = firstCheckPoint;
+            }
+            else{
+                
+                //Try to find the checkpoint on the list
+                CheckPoint* c = firstCheckPoint;
+                bool isCheckPointRegistered = false; // Avoid to introduce the first twice
+                
+                while(c->nextCheckPoint){   
+                    /*                             
+                    if ((*c->nextCheckPoint).x == (*firstCheckPoint).x && (*c->nextCheckPoint).y == (*firstCheckPoint).y){
+                        firstLapCompleted = true;
+                        c->nextCheckPoint->nextCheckPoint = firstCheckPoint; // Make the list circular to loop through
+                    }
+                    */                                           
+                    if ((*c->nextCheckPoint).x == next_checkpoint_x && (*c->nextCheckPoint).y == next_checkpoint_y){
+                        isCheckPointRegistered = true;
+                    }                
+                    c = c->nextCheckPoint;
+                }            
+                cerr << "isCheckPointRegistered " << isCheckPointRegistered << endl;
+                if (!isCheckPointRegistered){ // Register a new checkPoint
+                    lastCheckPointInserted->nextCheckPoint = new CheckPoint(next_checkpoint_x, next_checkpoint_y);
+                    lastCheckPointInserted = lastCheckPointInserted->nextCheckPoint;
+                }                       
+            }            
+        }
 
+        CheckPoint* c = firstCheckPoint;        
+        while(c->nextCheckPoint){
+            cerr <<"CheckPoint: x-> " << c->x << " y-> " << c->y << endl;
+            c = c->nextCheckPoint;
+        }
+        int absCheckpointAngle = abs(next_checkpoint_angle);
 
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
+        int thrust;
+        if (next_checkpoint_dist < 1500)
+            thrust = 20;
+        else 
+            thrust = 100 - clamp(absCheckpointAngle, 0, 180);
 
-        int thrust = (abs(next_checkpoint_angle) < 30) &&  (next_checkpoint_dist > 300) ? 100 : 20;
+        thrust = clamp(thrust, 20, 100);
+
+        cerr << "Thrust: " << thrust << endl;
         cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << endl;
+    }
+     CheckPoint* c = firstCheckPoint;        
+    
+    while(c->nextCheckPoint){ //Clean
+        CheckPoint* next = c->nextCheckPoint;
+        delete c;
+        c = next;
     }
 }
